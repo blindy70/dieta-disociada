6e3977ca5a21fd259fa788c664be1cdd479a2173
blindy70 <ddelasheras@gmail.com>
2026-08-30 23:24:09 +0200

    Lista de compra: mover el contador de veces entre el checkbox y el nombre del alimento

--- js/app.js
+++ js/app.js
@@ -584,8 +584,8 @@
       var veces = items[k] || 0;
       html += '<div class="checklist-item' + (done ? ' checked' : '') + '">';
       html += '<input type="checkbox" data-item="' + escapeAttr(k) + '"' + (done ? ' checked' : '') + '>';
-      html += '<span>' + escapeHtml(k) + '</span>';
       html += '<span class="qty" title="Veces que aparece en la dieta seleccionada">' + veces + '×</span>';
+      html += '<span>' + escapeHtml(k) + '</span>';
       html += '</div>';
     });
     html += '<p class="hint">Marca los ingredientes a medida que los compras. La lista se guarda en este dispositivo.</p>';
